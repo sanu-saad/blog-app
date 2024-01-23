87bb0b11e6c093b8e607ba23ecd7e5c09a0b8478
sanu <sanusaad444@gmail.com>
2024-01-23 18:53:28 +0530

    add git in backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,42 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App.jsx'
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Root from "./components/Root";
-import Home from "./components/Home";
-import About from "./components/about";
-import Signup from "./Signup";
-import Login from "./Login";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-
-      {
-        path: "signup",
-        element: <Signup />,
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-    ],
-  },
-]);
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
