bf8b68318c13c4a5916e74f03a5910b165115b19
sanu <sanusaad444@gmail.com>
2024-01-30 14:26:37 +0530

    register

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,48 @@
-const registerController = async (req, res) => {};
+import { hashPassword } from "../helpers/authHelper.js";
+import User from "../models/userModel.js";
 
-export { registerController };
+const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    if (!name || !email || !password || !phone || !address) {
+      return res.status(500).send({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    // CHECK USER
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(500).send({
+        success: false,
+        message: "User already exist",
+      });
+    }
+    //REGISTER USER
+    const hashedPassword = await hashPassword(password);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+      phone,
+      address,
+    });
+    await newUser.save();
+    return res.status(200).send({
+      success: true,
+      message: "User registered",
+      newUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in registration",
+      error,
+    });
+  }
+};
+
+const loginController = async (req, res) => {};
+
+export { registerController, loginController };
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
 const router = express.Router();
 
 //REGISTE || METHOD : POST
 router.post("/register", registerController);
+
+//LOGIN || METHOD : POST
+router.post("/login", loginController);
 
 export default router;
