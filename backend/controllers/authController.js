269461f7fedd429f1db70e13c445c1e3eeeed241
sanu <sanusaad444@gmail.com>
2024-01-30 15:12:09 +0530

    login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 const registerController = async (req, res) => {
   try {
@@ -43,6 +44,52 @@ const registerController = async (req, res) => {
   }
 };
 
-const loginController = async (req, res) => {};
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(500).send({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    //CHECK USER
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).send({
+        success: false,
+        message: "User not exist",
+      });
+    }
+    //PASSWORD MATCH
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(500).send({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+    //TOKEN
+    const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    return res.status(200).send({
+      success: true,
+      message: "User login successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token: token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in loginController",
+      error,
+    });
+  }
+};
 
 export { registerController, loginController };
